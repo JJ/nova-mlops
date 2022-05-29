a721a5a14d223df82b38adb82319107e7e594933
JJ Merelo <jjmerelo@gmail.com>
2022-05-29 11:36:58 +0200

    Testing models with security

---
+++ code/python/script/test-neptune-ai.py
@@ -0,0 +1,17 @@
+import neptune.new as neptune
+from dotenv import load_dotenv
+
+run = neptune.init(
+    project="jjmerelo/Nova",
+    api_token=os.getenv("NEPTUNE_API_TOKEN"),
+)  # your credentials
+
+params = {"learning_rate": 0.001, "optimizer": "Adam"}
+run["parameters"] = params
+
+for epoch in range(10):
+    run["train/loss"].log(0.9 ** epoch)
+
+run["eval/f1_score"] = 0.66
+
+run.stop()
