1b5f8b00381cdcc27e4b2a7b95c4958fadb0c277
JJ Merelo <jjmerelo@gmail.com>
2022-06-19 07:41:35 +0200

    Integrated bibliography in the generation of documentation
    Using R extensively, refs #8

--- bin/rmd-render.R
+++ bin/rmd-render.R
@@ -6,5 +6,5 @@ args = commandArgs(trailingOnly=TRUE)
 print(args)
 render(args[1],
        output_format = "md_document",
-       output_dir = ".",
+       output_dir = "text",
        output_file=paste0(args[1],".out"))
