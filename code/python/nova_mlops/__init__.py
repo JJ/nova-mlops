822332ba4979d45b0ba5a9ef318b7e593156d9da
JJ Merelo <jjmerelo@gmail.com>
2022-07-08 20:16:37 +0200

    Modularize

--- code/python/nova_mlops/__init__.py
+++ code/python/nova_mlops/__init__.py
@@ -5,11 +5,20 @@ from pycaret.regression import *
 import pandas as pd
 
 
-def train():
-    train = pd.read_parquet('../../data/ukr-mod-deltas.parquet')
-    s = setup(train, target='Losses', silent=True,
+def train(training_data):
+    s = setup(training_data, target='Losses', silent=True,
               log_experiment=True, experiment_name='test')
     best = compare_models()
     evaluate_model(best)
     save_model(best, "test")
     plot_model(best, plot='residuals_interactive')
+
+
+def process(ipd):
+    return ipd.drop(columns="Date")
+
+
+def go():
+    raw = pd.read_parquet('../../data/ukr-mod-deltas.parquet')
+    training_data = process(raw)
+    train(training_data)
